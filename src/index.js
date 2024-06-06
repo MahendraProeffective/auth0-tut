14247680be63405caa40cd5df3faba28fc9b2ac2
Mahendra Goud <Mahendra.Goud@proeffective.in>
2024-06-06 16:34:56 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Auth0Provider
+    domain="dev-htr72d31rop41wjz.us.auth0.com"
+    clientId="rRP3GmNBD7t02K7VVLporh4embraf6Y7"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
